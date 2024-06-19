589ea1fd9670aaaab946da35814a33e18894832f
Keugon <fdraxler@gmx.at>
2024-06-19 10:58:08 +0200

    start gework von artikel->entlehnung zu artikel->InventarGegenstand->Entlehnung

--- WIFI.Buchandlung.Client/Models/InventarArtikel.cs
+++ WIFI.Buchandlung.Client/Models/InventarArtikel.cs
@@ -6,13 +6,10 @@ using System.Threading.Tasks;
 
 namespace WIFI.Buchandlung.Client.Models
 {
-    public class InventarArtikel : WIFI.Buchandlung.Client.Models.Artikel
+    public class InventarGegenstand :System.Object
     {
-        public Guid Ausleiher { get; set; }
-        public DateTime AusleihDatum { get; set; }
-        public DateTime RückgabeDatum { get; set; }
-        public string? RückgabeZustand { get; set; }
-        public decimal Strafbetrag { get; set; }
-        public string? StrafbetragBemerkung { get; set; }
+        public Artikel? Artikel { get; set; }
+        public Entlehnung? Entlehnung { get; set; }
+       
     }
 }
